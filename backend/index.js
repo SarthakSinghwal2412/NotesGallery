5cae52704666aba80e2f9227305e598107a7fff0
SarthakSinghwal2412 <sarthaksinghwal2412@gmail.com>
2022-11-19 19:00:29 +0530

    another commit

--- backend/index.js
+++ backend/index.js
@@ -9,4 +9,8 @@ app.get("/",(req,res)=>{
 app.get("/api/notes",(req,res)=>{
     res.send("API is running...");
 })
+app.get('/api/notes/:id',(req,res)=>{
+    const note = notes.find((n) => n._id === req.params.id);
+    res.send(note);
+})
 app.listen(5000,console.log("Server is running at port 5000"));
