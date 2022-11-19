7e8b36f696a7ac48e4cf37c2b46ca7b227dce234
SarthakSinghwal2412 <sarthaksinghwal2412@gmail.com>
2022-11-19 18:54:22 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,12 @@
 const express = require('express');
+const notes = require("./data/notes");
+
 const app = express();
 
+app.get("/",(req,res)=>{
+    res.send("API is running...");
+})
+app.get("/api/notes",(req,res)=>{
+    res.send("API is running...");
+})
 app.listen(5000,console.log("Server is running at port 5000"));
